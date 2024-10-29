7a02380502a673a360a9cbcdaeaefe6e76a2fac2
willian <williancardoso00004@gmail.com>
2024-10-29 17:52:25 -0400

    Feat: config daatabase firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,18 +5,16 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyCYzE4JPa3LGK64HC1l3lQV6OzMHZ1tFXQ",
     authDomain: "testeaaateste.firebaseapp.com",
     projectId: "testeaaateste",
     storageBucket: "testeaaateste.appspot.com",
     messagingSenderId: "959615903063",
     appId: "1:959615903063:web:22eaa0766ed7e1a1848a12"
-
-};
+  };
 
 
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = await getFirestore(app);
