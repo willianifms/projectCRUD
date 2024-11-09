d4cc7fd2608a37fe5a9c909d2f2dda320ada6881
willian <williancardoso00004@gmail.com>
2024-11-09 12:42:42 -0400

    Fix: adapt project build vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Rotas from "./routes.tsx"
+import Rotas from "./routes"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
